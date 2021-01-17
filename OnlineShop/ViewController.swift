8bd77c54eaa54faa5b977582412abbd0ea9cea30
alexlarin <alex12081973@mail.ru>
2021-01-17 21:35:45 +0300

    init

---
+++ OnlineShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineShop
+//
+//  Created by Alex Larin on 17.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
